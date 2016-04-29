6d86ba5db52d6d42b64e8918e67d5bc30f218d2d
Kyle Pittman <pittman.kyle@gmail.com>
2016-04-29 12:33:19 -0500

    Update __init__.py

--- Beheaded/__init__.py
+++ Beheaded/__init__.py
@@ -13,8 +13,7 @@ import time
 
 from Runner import Run
 
-logger = logging.getLogger(__name__)
-logging.basicConfig()
+logger = logging.getLogger('root')
 
 
 class UnableToFindFreeDisplayException(Exception):
